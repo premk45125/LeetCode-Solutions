091fcaa002326bc36ae5d33e4cb7c5e57d6402c0
Prem Kumar <premkumaryadavchikna@gmail.com>
2026-07-12 17:05:25 +0530

    Time: 0 ms (100%), Space: 9.4 MB (35.73%) - LeetHub

---
+++ 0859-buddy-strings/0859-buddy-strings.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool buddyStrings(string s, string goal) {
+        if (s.length() != goal.length())
+            return false;
+
+        if (s == goal) {
+            vector<int> freq(26, 0);
+            for (char c : s) {
+                if (++freq[c - 'a'] > 1)
+                    return true;
+            }
+            return false;
+        }
+
+        vector<int> diff;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] != goal[i])
+                diff.push_back(i);
+        }
+
+        return diff.size() == 2 &&
+               s[diff[0]] == goal[diff[1]] &&
+               s[diff[1]] == goal[diff[0]];
+    }
+};
