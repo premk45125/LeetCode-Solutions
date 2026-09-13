c115b6871b6a6b794d4085064e956e599da95542
Prem Kumar <premkumaryadavchikna@gmail.com>
2026-09-13 19:38:21 +0530

    Time: 0 ms (100%), Space: 16 MB (90.05%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -1,18 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int idx = 0;
-
-    TreeNode* build(vector<int>& preorder, int bound = INT_MAX) {
-        if (idx == preorder.size() || preorder[idx] > bound) 
-            return nullptr;
-
-        TreeNode* root = new TreeNode(preorder[idx++]);
-        root->left = build(preorder, root->val);   // left subtree must be < root->val
-        root->right = build(preorder, bound);      // right subtree must be < bound
-        return root;
-    }
-
     TreeNode* bstFromPreorder(vector<int>& preorder) {
-        return build(preorder);
+        int n=preorder.size();
+        // vector<int>inorder=preorder;
+        // sort(inorder.begin(),inorder.end());
+        TreeNode* root=new TreeNode(preorder[0]);
+        TreeNode* current=root;
+        stack<TreeNode*>st;
+        st.push(current);
+        for(int i=1;i<n;i++){
+            TreeNode* node=new TreeNode(preorder[i]);
+            if(node->val < st.top()->val){
+                current->left=node;
+                current=node;
+            }
+            else{
+                TreeNode* temp=st.top();
+                while(!st.empty() && st.top()->val < node->val ){
+                    temp=st.top();
+                    st.pop(); 
+                }
+                temp->right=node;
+                current=node;
+            }
+                st.push(node);
+
+        }
+        
+
+        return root;
     }
 };
